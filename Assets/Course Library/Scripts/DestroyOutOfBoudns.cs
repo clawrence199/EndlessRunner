8d1f6ec612666d62613405b827427f6ed04e7806
Cory Lawrence <35942282+clawrence199@users.noreply.github.com>
2020-07-23 21:03:22 -0500

    Initial Commit

---
+++ Assets/Course Library/Scripts/DestroyOutOfBoudns.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBoudns : MonoBehaviour
+{
+    private float topBound = 50.0f;
+    private float lowerBound = -20.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Destroy objects when they go off screen 
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        } else if (transform.position.z < lowerBound)
+        {
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10.0f;
+
+    public GameObject projectilePrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Set left bounds 
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        //Set right bounds
+        if (transform.position.x > 10)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        //Move the player 
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        //Use spacebar to fire projectile prefab
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+           
+    }
+}
